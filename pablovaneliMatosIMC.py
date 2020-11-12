7221c573faedcf615c855a17220042ae3e153e77
Pablo Vaneli <65033861+Dexterv@users.noreply.github.com>
2020-11-12 19:42:38 -0300

    Add files via upload

---
+++ pablovaneliMatosIMC.py
@@ -0,0 +1,6 @@
+peso=int(input("Por favor,(em Kg) entre com o seu peso.:"))
+altura=int(input("Por favor,(em Metros) entre com o a sua altura.:"))
+
+imc= peso/(altura**2)
+
+print("Seu IMC é:",imc,)
